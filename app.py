325a323336409a5db3d1655ce41f51e22c738fe0
floram01 <florentrambaud@gmail.com>
2016-08-06 18:43:53 +0200

    add Procfile

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_from_directory, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def main():
+  return send_from_directory('.','static/viz/summer_olympics/index.html')
+
+if __name__=="__main__":
+  app.run(debug=True)
