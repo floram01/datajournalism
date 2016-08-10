1b510d1054eaf546c3c0b9a98f7221f92751df14
floram01 <florentrambaud@gmail.com>
2016-08-10 15:59:55 +0200

    add simple navigation

--- app/views.py
+++ app/views.py
@@ -2,9 +2,23 @@ from flask import Flask, send_from_directory, render_template
 from app import app
 
 @app.route('/')
-def last_article():
-  return render_template('nobel.html')
-
-# @app.route('/')
-# def previous_article():
-#   return render_template('nobel.html')
+@app.route('/summer_olympics')
+def summer_olympics():
+    title="JO 2016 top 10 meilleures disciplines par pays"
+    previous_article={'url':'/nobel', 'title':'Prix nobels'}
+    next_article={}
+    return render_template(
+                         'summer_olympics.html',
+                         previous_article=previous_article,
+                         next_article=next_article
+                         )
+@app.route('/nobel')
+def nobel():
+    title="JO 2016 top 10 meilleures disciplines par pays"
+    previous_article={}
+    next_article={'url':'/summer_olympics', 'title':"JO 2016 top 10 meilleures disciplines par pays"}
+    return render_template(
+                         'nobel.html',
+                         previous_article=previous_article,
+                         next_article=next_article
+                         )
