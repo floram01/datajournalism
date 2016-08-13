2aca6b224ac9d512357bc1da1c76572661c0da22
floram01 <florentrambaud@gmail.com>
2016-08-13 16:32:46 +0200

    add auto generation views from archive file + auto upate archive when creating new project

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,47 @@
 from flask import Flask, render_template
+import pandas as pd
+import sys
+
+sys.path.insert(0,'app/')
 
 app = Flask(__name__)
 app.config.from_object('config')
 app.TITLE = 'Weekly Toucan'
 
-#a mettre en place : creation auto d'une view pour chaque nouveau projet
-#il faut ajouter dans la base mongo, utilities, collec = views
-#la db mongo pourrait etre utilisee plus largement pour contenir les param data_params et parameters.js
-#data_params peut ensuite etre utilise depuis mongo db sans s'emmerder avec les path
 
-#view = {url:, title:,previous_article:{}, next_article:{}}
-#insert existing views in mongo (use indexing)
-#build a method to add new article in mongo
-#build a method to update previous/next article info(use indexing)
-#use below method to build all views based on what's contained in the database ==> complusory to move to full db
-#==> do that alternatively, have a json/csv file with that information, and update it as needed (use as a db)
-
-previous_article={'url':'/nobel', 'title':'Prix nobels'}
-next_article={}
-PROJECT_NAME = 'summer_olympics'
-
-def function_maker(previous_article, next_article, PROJECT_NAME):
+def function_maker(previous_article, next_article, template_name):
     def my_function():
         return render_template(
-                               PROJECT_NAME + '.html',
+                               template_name,
                                previous_article=previous_article,
                                next_article=next_article,
                                title=app.TITLE
                                )
     return my_function
 
-my_index = function_maker(previous_article, next_article, PROJECT_NAME)
-app.add_url_rule('/' + PROJECT_NAME, PROJECT_NAME, my_index)
+df = pd.read_csv('app/archive.csv')
+
+for i in df.index :
+    project_name = df.loc[:, 'project_name'][i]
+    template_name = df.loc[:, 'template_name'][i]
+    previous_url = df.loc[:, 'previous_url'][i]
+    next_url = df.loc[:, 'next_url'][i]
+    
+    app.add_url_rule(
+                     '/' + project_name,
+                     project_name,
+                     function_maker(previous_url, next_url, template_name)
+                     )
+
+latest_id = df.index[-1]
+template_name = df.loc[:, 'template_name'][latest_id]
+previous_url = df.loc[:, 'previous_url'][latest_id]
+next_url = df.loc[:, 'next_url'][latest_id]
+
+app.add_url_rule(
+                 '/',
+                 'latest',
+                 function_maker(previous_url, next_url, template_name)
+                 )
 
 from app import views
--- make_new_project.py
+++ make_new_project.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import sys, shutil,os
+import pandas as pd
 sys.path.insert(0,'data/boiler_plate')
 sys.path.insert(0,'data/data_tools/ressources')
 sys.path.insert(0,'app/templates/')
+sys.path.insert(0,'app/')
+
 from my_logger import logger
 from app import app
 from flask import render_template
@@ -11,7 +14,26 @@ def run(PROJECT_NAME):
     prepare_data_params(PROJECT_NAME)
     prepare_template(PROJECT_NAME)
     prepare_static(PROJECT_NAME)
-    logger.info("don't forget to manually update the app>static>views.py (see instructions in the file)")
+    update_archive(PROJECT_NAME)
+
+def update_archive(PROJECT_NAME):
+    df = pd.read_csv('app/archive.csv')
+    if PROJECT_NAME in df.project_name.unique():
+            logger.info('There already is a project named ' + PROJECT_NAME + ', please choose another name')
+            sys.exit()
+    last_id = df.index[-1] 
+    previous_url = df.loc[:, 'project_name'][last_id]
+    
+    df = df.append({
+              'project_name':PROJECT_NAME,
+              'template_name':PROJECT_NAME + '.html',
+              'previous_url':previous_url,
+              },
+              ignore_index = True
+              )
+
+    df.loc[df.index == last_id,'next_url'] = PROJECT_NAME
+    df.to_csv('app/archive.csv', index=False)
 
 def prepare_static(PROJECT_NAME):
     try:
